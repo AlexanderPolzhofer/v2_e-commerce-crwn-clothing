44fbd2abc9c509ba06554f2f29f7cd11c635a3c7
AlexanderPolzhofer <alexander.polzhofer@gmail.com>
2022-03-31 19:47:49 +0200

    adding routes and nested routes, logo and styles  to navigationbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import Home from './routes/home/Home.jsx';
 
+import { Routes, Route } from 'react-router-dom';
+
+import NavigationBar from './routes/navbar/NavigationBar.jsx';
+
+import ShopPage from './routes/shop/ShopPage.jsx';
+
 const App = () => {
 
-
-  return <Home />;
+  return (
+    <Routes>
+      <Route path='/' element={<NavigationBar />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<ShopPage />} />
+      </Route>
+    </Routes>);
 }
 
 export default App;
---
+++ src/routes/shop/ShopPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ShopPage = () => {
+    return (
+        <div>
+            <p>I am the shop page</p>
+        </div>)
+}
+export default ShopPage;
