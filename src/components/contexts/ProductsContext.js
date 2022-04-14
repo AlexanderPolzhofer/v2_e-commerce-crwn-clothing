896e23ebe18a2ceebcce7bee1805d5df98fcc233
AlexanderPolzhofer <alexander.polzhofer@gmail.com>
2022-04-14 22:34:38 +0200

    add shop data to firestore db, get data from firestore db

--- src/components/contexts/ProductsContext.js
+++ src/components/contexts/ProductsContext.js
@@ -1,4 +1,5 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { addCollectionAndDocuments, getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 
 import PRODUCTS from './../../shop-data';
 
@@ -7,8 +8,27 @@ export const ProductsContext = createContext({
 });
 
 export const ProductsContextProvider = ({ children }) => {
-    const [products, setProducts] = useState(PRODUCTS);
-    const value = { products, setProducts }
+    const [products, setProducts] = useState([]);
+
+    // add collection and documents to firestore
+
+
+    /* useEffect(() => {
+            addCollectionAndDocuments('categories', PRODUCTS);
+            console.log('done')
+        }, []) 
+        */
+
+
+    useEffect(() => {
+        const getCategoriesMap = async () => {
+            const categoryMap = await getCategoriesAndDocuments();
+            console.log(categoryMap)
+        }
+        getCategoriesMap();
+    }, []);
+
+    const value = { products }
 
     return <ProductsContext.Provider value={value}>
         {children}
