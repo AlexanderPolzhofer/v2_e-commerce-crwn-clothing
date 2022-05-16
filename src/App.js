b6006cac12a5fbcabdf239fc0e066e9fe758c028
AlexanderPolzhofer <alexander.polzhofer@gmail.com>
2022-05-16 18:52:00 +0200

    converting onAuthStateChangedListener to promise, check user session saga

--- src/App.js
+++ src/App.js
@@ -1,33 +1,21 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-
-import { setCurrentUser } from './store/user/user.action.js';
-
-import {
-  onAuthStateChangedListener,
-  createUserDocumentFromAuth
-} from './utils/firebase/firebase.utils.js';
+import { Routes, Route } from 'react-router-dom';
 
 import Home from './routes/home/Home.jsx';
-import { Routes, Route } from 'react-router-dom';
 import NavigationBar from './routes/navbar/NavigationBar.jsx';
 import ShopPage from './routes/shop/ShopPage.jsx';
 import Authentication from './routes/authentication/Authentication.jsx';
 import Checkout from './routes/checkout/Checkout.jsx';
+
+import { checkUserSession } from './store/user/user.action.js';
 
 const App = () => {
 
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChangedListener((user) => {
-      if (user) {
-        createUserDocumentFromAuth(user);
-      }
-      dispatch(setCurrentUser(user));
-    });
-
-    return unsubscribe;
+    dispatch(checkUserSession())
   }, []);
 
   return (
