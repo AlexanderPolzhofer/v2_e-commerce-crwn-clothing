d47f62a784c1948fc1d89c8b18770a8b86d0de0d
AlexanderPolzhofer <alexander.polzhofer@gmail.com>
2022-05-21 23:45:23 +0200

    updatin paymentIntent function, init netlify dev server with cli

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -21,14 +21,13 @@ const PaymentForm = () => {
             return;
         }
 
-        const response = await fetch('/.netlify/functions/create-payment-intent.js', {
+        const response = await fetch('/.netlify/functions/create-payment-intent', {
             method: 'post',
             headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
+                'Content-Type': 'application/json'
             },
             body: JSON.stringify({ amount: 10000 })
-        }).then(res => res.json())
+        }).then((res) => res.json())
 
         console.log(response)
 
