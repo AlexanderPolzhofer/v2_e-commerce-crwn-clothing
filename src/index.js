49e40c3e6784cb9ddb42ec24374a7e49fae0d198
AlexanderPolzhofer <alexander.polzhofer@gmail.com>
2022-06-14 16:35:44 +0200

    updating code to be pwa compliant, added serviceworker, https, update manifest

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import { stripePromise } from './utils/stripe/stripe.utils';
 import { Elements } from '@stripe/react-stripe-js';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import * as serviceWorker from './service-worker';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -34,3 +34,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorker.register();
